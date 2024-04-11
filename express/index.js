8e25e3cc74b6ee3b1f983a4379cd4e9b6f5c5331
heeju <hhj05230@gmail.com>
2024-04-11 15:49:22 +0900

    path

---
+++ express/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+app.use("/html2", express.static(path.join(__dirname, "html")))
+
+app.get('/', function (req, res){
+    console.log("경로 : ", path.join(path.dirname, "html"))
+    res.send('Hello World')
+})
+
+app.get('/apple', function (req, res){
+    res.send('사과')
+})
+
+app.get('/home', function (req, res){
+    // res.send(__dirname)
+    res.sendFile(__dirname)
+})
+app.get('/grape/:name', function (req, res){
+    console.log('path', req.path)
+    console.log('params', req.params)
+    console.log('query', req.query)
+    res.send("포도페이지입니다.")
+})
+
+
+app.listen(3000);
