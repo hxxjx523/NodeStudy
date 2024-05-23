da76eccd6ab4e3bc47082227ee20aad9090f2d30
heeju <hhj05230@gmail.com>
2024-05-23 14:55:25 +0900

    보드

--- board/board.js
+++ board/board.js
@@ -1,15 +1,22 @@
-const express = require(express);
+const express = require("express");
 const app = express();
 
 
 
 
 const dotenv = require("dotenv").config();
-app.use(morgan('dev'));
 
 const morgan = require("morgan");
+app.use(morgan('dev'))
+
 const cors = require("cors");
 app.use(cors({
     origin: "http://127.0.0.1:5500",
     credentials:true
-}))
+}));
+
+app.set('port', process.env.PORT);
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'), "번 포트에서 서버 실행");
+});
